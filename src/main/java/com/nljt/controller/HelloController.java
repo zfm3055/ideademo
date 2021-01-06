45f291afd156cbe5b4b58c4b40af20656e44cbb0
zhangfengming <zfm305@163.com>
2021-01-06 17:39:02 +0800

    首次提交

---
+++ src/main/java/com/nljt/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.nljt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/info")
+    public String info(){
+        return "hello zfm";
+    }
+}
